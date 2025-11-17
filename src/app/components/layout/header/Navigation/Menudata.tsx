ecfcee09a149a1a6302bf0a4ab0c06a0d5ae6495
roro <xwang.robert@gmail.com>
2025-11-17 14:01:29 +1100

    Nav bar updated

--- src/app/components/layout/header/Navigation/Menudata.tsx
+++ src/app/components/layout/header/Navigation/Menudata.tsx
@@ -4,8 +4,5 @@ export const headerData: HeaderItem[] = [
     { label: 'About us', href: '/#aboutus' },
     { label: 'Services', href: '/#services' },
     { label: 'Work', href: '/#work' },
-    { label: 'Team', href: '/#team' },
-    { label: 'Pricing', href: '/#pricing' },
-    { label: 'Awards', href: '/#awards' },
     { label: 'Contact', href: '/contact' },
 ];
